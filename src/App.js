aa47423e7195ad5ba16364b9e038435d60ad90f3
dickm19 <maryclairedick@gmail.com>
2021-09-20 23:37:21 -0400

    tried adding routes so user can get a shareable link for a post but calling it a night

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import { Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import Post from './Post'
 import './App.css';
 
@@ -6,7 +9,8 @@ import './App.css';
 class App extends Component {
 
   state = {
-    data: []
+    data: [],
+    numberedData: []
   }
 
   fetchPosts = () => {
@@ -16,12 +20,43 @@ class App extends Component {
   }
 
   renderPosts = () => {
-    if (this.state.data.count !== 0){
+    if (this.state.numberedData){
       
-      
-      return this.state.data.map(post => {
-        return <Post imageUrl={post["links"][0]["href"]} title={post["data"][0]["title"]} description={post["data"][0]["description"]} dateCreated={post["data"][0]["date_created"]} likes={0} key={post["data"][0].nasa_id}></Post>
+      let count = 0
+      return this.numberPosts().map(post => {
+        let newPost = <Post imageUrl={post.imageUrl} title={post.title} description={post.description} dateCreated={post.dateCreated} likes={0} id={count} key={count}></Post>
+        count++
+        return newPost
      } )
+     
+    }
+  }
+
+  numberPosts = () => {
+      let count = 0
+      return this.state.data.map(post => {
+
+        let newPost = {
+          imageUrl: post["links"][0]["href"],
+          title: post["data"][0]["title"],
+          description: post["data"][0]["description"],
+          dateCreated: post["data"][0]["date_created"],
+          likes: 0,
+          id: count
+        }
+         
+        count++
+        return newPost
+      })
+      // this.setState({numberedData: numberedData })
+
+  }
+
+  renderRoutes = () => {
+    if (this.state.numberedData){
+      return this.state.numberedData.map(post => {
+        return <Route exact path={`/${post.id}`} component={<Post imageUrl={post.imageUrl} title={post.title} description={post.description} dateCreated={post.dateCreated} likes={0} id={post.id} key={post.id}></Post>}></Route>
+      })
     }
   }
 
@@ -30,17 +65,22 @@ class App extends Component {
     this.fetchPosts()
   }
 
-
   render(){
     return (
-    <div className="App">
-      <h1 className='header'>
-        Spacestagram
-      </h1>
-      
-      {this.renderPosts()}
-      
-    </div>
+      <BrowserRouter>
+          <div className="App">
+            <h1 className='header'>
+              Spacestagram
+            </h1>
+          <Route
+                exact
+                path="/"
+                render={this.renderPosts}
+            />
+            
+          </div>
+          {this.renderRoutes()}
+      </BrowserRouter>
     )
   }
 }
--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom'
+import { Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './Post.css'
 
 export default class Post extends Component{
@@ -24,17 +28,22 @@ export default class Post extends Component{
 
     render(){
         return(
-            <div className='post'>
-                <img className='galaxy' src={this.props.imageUrl} alt={this.props.title}></img>
-                <br></br>
-                <h3 className='title'>{this.props.title}</h3>
-                <p className="description">{this.props.description}</p>
-                <p>{this.props.dateCreated}</p>
-                <div className="like-button">
-                    <img className="heart" onClick={this.handleLike} src={this.state.clicked ? "https://i.ibb.co/ncrr1M3/Pik-Png-com-undertale-heart-png-790472.png" : "https://i.ibb.co/9gVkfmd/Pik-Png-com-pixel-heart-png-971526.png"} alt='heart'></img>
-                    <p>{this.state.likes}</p>
+            <BrowserRouter>
+                <div className='post'>
+                    <Link to={`/${this.props.id}`}>
+                        <img className='galaxy' src={this.props.imageUrl} alt={this.props.title}></img>
+
+                    </Link>
+                    <br></br>
+                    <h3 className='title'>{this.props.title}</h3>
+                    <p className="description">{this.props.description}</p>
+                    <p>{this.props.dateCreated}</p>
+                    <div className="like-button">
+                        <img className="heart" onClick={this.handleLike} src={this.state.clicked ? "https://i.ibb.co/ncrr1M3/Pik-Png-com-undertale-heart-png-790472.png" : "https://i.ibb.co/9gVkfmd/Pik-Png-com-pixel-heart-png-971526.png"} alt='heart'></img>
+                        <p>{this.state.likes}</p>
+                    </div>
                 </div>
-            </div>
+            </BrowserRouter>
         )
     }
 }
