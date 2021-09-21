b9d0662250d14512ded9d5f1db8ed874bc378151
dickm19 <maryclairedick@gmail.com>
2021-09-20 21:23:58 -0400

    added drop shadow

--- src/Post.js
+++ src/Post.js
@@ -28,7 +28,7 @@ export default class Post extends Component{
                 <img className='galaxy' src={this.props.imageUrl} alt={this.props.title}></img>
                 <br></br>
                 <h3 className='title'>{this.props.title}</h3>
-                <p>{this.props.description}</p>
+                <p className="description">{this.props.description}</p>
                 <p>{this.props.dateCreated}</p>
                 <div className="like-button">
                     <img className="heart" onClick={this.handleLike} src={this.state.clicked ? "https://i.ibb.co/ncrr1M3/Pik-Png-com-undertale-heart-png-790472.png" : "https://i.ibb.co/9gVkfmd/Pik-Png-com-pixel-heart-png-971526.png"} alt='heart'></img>
